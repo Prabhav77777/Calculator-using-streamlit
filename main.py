2498ed3a7890ca89dd68ce028ef9105d47938be4
Prabhav Agrawal <prabhavagrawal2007@gmail.com>
2026-07-06 22:13:17 +0530

    add

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ import streamlit as st
 st.header("Calculator")
 firstnumber=st.text_input("enter the first number")
 secondnumber=st.text_input("enter the second number")
+if st.button("ADD"):
+
+    st.write(f"The Addition is {int(firstnumber)+int(secondnumber)}")
+
